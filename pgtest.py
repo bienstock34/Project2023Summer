1d1e7a29d8accdad3d949098744448b52c52c040
Samson Bienstock <samsonbienstock@Samsons-MacBook-Pro.local>
2023-07-26 14:08:54 +0800

    Itnitial commit: Add pgtest.py

---
+++ pgtest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print(3+4)
+try:
+    import pygcode
+    print("pygcode is installed correctly.")
+except ImportError:
+    print("pygcode is not installed.")
+
+
+# In[ ]:
+
+
+
+
